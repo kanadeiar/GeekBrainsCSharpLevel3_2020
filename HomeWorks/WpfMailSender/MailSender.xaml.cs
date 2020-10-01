e06832799d0de4861703a1d05806a0ced2c9e877
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-01 04:40:38 +0300

    Кнопка отправки письма

--- HomeWorks/WpfMailSender/MailSender.xaml.cs
+++ HomeWorks/WpfMailSender/MailSender.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using WpfMailSender.Models;
+using WpfMailSender.Services;
 
 namespace WpfMailSender
 {
@@ -16,6 +20,29 @@ namespace WpfMailSender
         private void MenuItem_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+
+        private void ButtonSendNow_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ComboBoxSenders.SelectedItem is Sender sender_)) return;
+            if (!(DataGridRecirients.SelectedItem is Recipient recipient)) return;
+            if (!(ComboBoxServers.SelectedItem is Server server)) return;
+            if (!(ListBoxMessages.SelectedItem is Message message)) return;
+            
+            var mailSender = new SmtpSender(server.Address, server.Port, server.UseSSL, server.Login, server.Password);
+
+            try
+            {
+                var timer = Stopwatch.StartNew();
+                mailSender.Send(sender_.Address, recipient.Address, message.Title, message.Body);
+                timer.Stop();
+                MessageBox.Show($"Почтовое сообщение успешно отправлено за {timer.Elapsed.TotalSeconds:0.##} секунд", "Отправка почты", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка отправки почты!", "Отправка почты", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
