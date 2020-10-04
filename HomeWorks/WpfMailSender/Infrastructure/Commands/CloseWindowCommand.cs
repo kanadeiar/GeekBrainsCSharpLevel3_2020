111b2a6250f798b4fcdbef8c71f5a5e482f9fe92
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-04 20:51:12 +0300

    Команды работают +

---
+++ HomeWorks/WpfMailSender/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Windows;
+using WpfMailSender.Infrastructure.Commands.Base;
+
+namespace WpfMailSender.Infrastructure.Commands
+{
+    class CloseWindowCommand : Command
+    {
+        protected override void Execute(object p)
+        {
+            var window = p as Window;
+            if (window is null)
+                window = App.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(w => w.IsFocused);
+            if (window is null)
+                window = App.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(w => w.IsActive);
+            window?.Close();
+        }
+    }
+}
--- HomeWorks/WpfMailSender/ViewModels/MainWindowViewModel.cs
+++ HomeWorks/WpfMailSender/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using WpfMailSender.ViewModels.Base;
+﻿using System.Windows;
+using System.Windows.Input;
+using WpfMailSender.Infrastructure.Commands;
+using WpfMailSender.ViewModels.Base;
 
 namespace WpfMailSender.ViewModels
 {
@@ -14,8 +17,14 @@ namespace WpfMailSender.ViewModels
             set => Set(ref _title, value);
         }
 
-
-
-
+        #region Команды
+        private ICommand _showDialogCommand;
+        public ICommand ShowDialogCommand => _showDialogCommand ?? (_showDialogCommand = new LambdaCommand(OnShowDialogCommandExecuted));
+        private void OnShowDialogCommandExecuted(object p)
+        {
+            var message = p as string ?? "Привет, Мир!";
+            MessageBox.Show(message, "Сообщение приложения", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        #endregion
     }
 }
