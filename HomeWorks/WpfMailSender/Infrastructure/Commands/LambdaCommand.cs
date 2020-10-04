2c655657d7490fe79164f6d0a87a8f518e08d8eb
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-04 18:16:06 +0300

    наконец-то + MVVM

---
+++ HomeWorks/WpfMailSender/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WpfMailSender.Infrastructure.Commands.Base;
+
+namespace WpfMailSender.Infrastructure.Commands
+{
+    class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        protected override bool CanExecute(object p) => _canExecute?.Invoke(p) ?? true;
+        protected override void Execute(object p) => _execute(p);
+    }
+}
---
+++ HomeWorks/WpfMailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using WpfMailSender.ViewModels.Base;
+
+namespace WpfMailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+        private string _title = "Рассыльщик электронной почты";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+
+
+
+    }
+}
