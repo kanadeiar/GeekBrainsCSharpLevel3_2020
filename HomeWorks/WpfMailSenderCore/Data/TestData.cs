3cb11d8360b42971ac60d0a26bf5e5ed9aa4da76
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-05 23:10:02 +0300

    Тестовые данные +

---
+++ HomeWorks/WpfMailSenderCore/Data/TestData.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WpfMailSender.Models;
+
+namespace WpfMailSenderCore.Data
+{
+    static class TestData
+    {
+        public static IList<Server> Servers { get; } = new List<Server>
+        {
+            new Server
+            {
+                Id = 1,
+                Name = "Яндекс",
+                Address = "smtp.yandex.ru",
+                Port = 465,
+                UseSSL = true,
+                Login = "user@yandex.ru",
+                Password = "Password",
+            },
+            new Server
+            {
+                Id = 2,
+                Name = "GMail",
+                Address = "smtp.gmail.com",
+                Port = 465,
+                UseSSL = true,
+                Login = "user@gmail.com",
+                Password = "Password",
+            }
+        };
+
+        public static IList<Sender> Senders { get; } = new List<Sender>
+        {
+            new Sender
+            {
+                Id = 1,
+                Name = "Иванов",
+                Address = "ivanov@server.ru",
+                Description = "Почта от Иванова"
+            },
+            new Sender
+            {
+                Id = 2,
+                Name = "Петров",
+                Address = "petrov@server.ru",
+                Description = "Почта от Петровича"
+            },
+            new Sender
+            {
+                Id = 3,
+                Name = "Сидоров",
+                Address = "sidorov@server.ru",
+                Description = "Почта от Сидорова"
+            }
+        };
+
+        public static IList<Recipient> Recipients { get; } = new List<Recipient>
+        {
+            new Recipient
+            {
+                Id = 1,
+                Name = "Иванов",
+                Address = "ivanov@server.ru",
+                Description = "Почта для Иванова"
+            },
+            new Recipient
+            {
+                Id = 2,
+                Name = "Петров",
+                Address = "petrov@server.ru",
+                Description = "Почта для Петрова"
+            },
+            new Recipient
+            {
+                Id = 3,
+                Name = "Сидоров",
+                Address = "sidorov@server.ru",
+                Description = "Почта для Сидорова"
+            }
+        };
+
+        public static IList<Message> Messages { get; } = Enumerable
+            .Range(1, 10)
+            .Select(i => new Message
+            {
+                Id = i,
+                Title = $"Сообщение {i}",
+                Body = $"Текст сообщения {i}"
+            })
+            .ToList();
+    }
+
+}
--- HomeWorks/WpfMailSenderCore/ViewModels/MainWindowViewModel.cs
+++ HomeWorks/WpfMailSenderCore/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Timers;
 using System.Windows;
 using System.Windows.Input;
+using WpfMailSender.Models;
+using WpfMailSenderCore.Data;
 using WpfMailSenderCore.Infrastructure.Commands;
 using WpfMailSenderCore.ViewModels.Base;
 
@@ -16,6 +19,7 @@ namespace WpfMailSenderCore.ViewModels
             _timer.AutoReset = true;
             _timer.Enabled = true;
         }
+        #region Свойства
         private string _title = "Рассыльщик электронной почты Core";
         public string Title
         {
@@ -28,7 +32,31 @@ namespace WpfMailSenderCore.ViewModels
         {
             OnPropertyChanged(nameof(CurrentTime));
         }
-
+        private ObservableCollection<Server> _servers;
+        public ObservableCollection<Server> Servers
+        {
+            get => _servers;
+            set => Set(ref _servers, value);
+        }
+        private ObservableCollection<Sender> _senders;
+        public ObservableCollection<Sender> Senders
+        {
+            get => _senders;
+            set => Set(ref _senders, value);
+        }
+        private ObservableCollection<Recipient> _recipients;
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _recipients;
+            set => Set(ref _recipients, value);
+        }
+        private ObservableCollection<Message> _messages;
+        public ObservableCollection<Message> Messages
+        {
+            get => _messages;
+            set => Set(ref _messages, value);
+        }
+        #endregion
         #region Команды
         private ICommand _showDialogCommand;
         public ICommand ShowDialogCommand => _showDialogCommand ??= new LambdaCommand(OnShowDialogCommandExecuted);
@@ -36,6 +64,15 @@ namespace WpfMailSenderCore.ViewModels
         {
             var message = p as string ?? "Привет, Мир!";
             MessageBox.Show(message, "Сообщение приложения", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private ICommand _loadDataCommand;
+        public ICommand LoadDataCommand => _loadDataCommand ??= new LambdaCommand(OnLoadDataCommandExecuted);
+        private void OnLoadDataCommandExecuted(object p)
+        {
+            Servers = new ObservableCollection<Server>(TestData.Servers);
+            Senders = new ObservableCollection<Sender>(TestData.Senders);
+            Recipients = new ObservableCollection<Recipient>(TestData.Recipients);
+            Messages = new ObservableCollection<Message>(TestData.Messages);
         }
         #endregion
     }
