5ce3ec7f3ac7c83a410c8f98977ce384f121b9f2
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-05 21:53:23 +0300

    Добавлена команда справка

--- HomeWorks/WpfMailSenderCore/ViewModels/MainWindowViewModel.cs
+++ HomeWorks/WpfMailSenderCore/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using WpfMailSenderCore.ViewModels.Base;
+﻿using System.Windows;
+using System.Windows.Input;
+using WpfMailSenderCore.Infrastructure.Commands;
+using WpfMailSenderCore.ViewModels.Base;
 
 namespace WpfMailSenderCore.ViewModels
 {
@@ -13,5 +16,15 @@ namespace WpfMailSenderCore.ViewModels
             get => _title;
             set => Set(ref _title, value);
         }
+
+        #region Команды
+        private ICommand _showDialogCommand;
+        public ICommand ShowDialogCommand => _showDialogCommand ??= new LambdaCommand(OnShowDialogCommandExecuted);
+        private void OnShowDialogCommandExecuted(object p)
+        {
+            var message = p as string ?? "Привет, Мир!";
+            MessageBox.Show(message, "Сообщение приложения", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        #endregion
     }
 }
