f10227d64ba2546fc5d181d6a62e45a2bc7eccfe
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-05 22:34:11 +0300

    Добавление сервис-локатора

--- HomeWorks/WpfMailSenderCore/App.xaml.cs
+++ HomeWorks/WpfMailSenderCore/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfMailSenderCore.ViewModels;
 
 namespace WpfMailSenderCore
 {
@@ -13,5 +16,21 @@ namespace WpfMailSenderCore
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Hosting;
+        public static IHost Hosting
+        {
+            get
+            {
+                if (__Hosting != null) return __Hosting;
+                var host_builder = Host.CreateDefaultBuilder(Environment.GetCommandLineArgs());
+                host_builder.ConfigureServices(ConfigureServices);
+                return __Hosting = host_builder.Build();
+            }
+        }
+        public static IServiceProvider Services => Hosting.Services;
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
