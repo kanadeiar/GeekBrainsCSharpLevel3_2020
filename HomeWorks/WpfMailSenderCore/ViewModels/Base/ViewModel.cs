c902b189baeb0405bfc42392d3ecc73676c5c23b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-05 21:42:36 +0300

    MVVM +

---
+++ HomeWorks/WpfMailSenderCore/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfMailSenderCore.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return;
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ HomeWorks/WpfMailSenderCore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using WpfMailSenderCore.ViewModels.Base;
+
+namespace WpfMailSenderCore.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+        private string _title = "Рассыльщик электронной почты Core";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
