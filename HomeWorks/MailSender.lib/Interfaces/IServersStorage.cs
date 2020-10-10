9156722a4bc965126c0f7741a4431702c3e41fb0
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-10 06:36:11 +0300

    Хранилище данных в памяти

---
+++ HomeWorks/MailSender.lib/Interfaces/IServersStorage.cs
@@ -0,0 +1,14 @@
+﻿using MailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Interfaces
+{
+    public interface IServersStorage
+    {
+        ICollection<Server> Servers { get; }
+        void Load();
+        void SaveChanges();
+    }
+}
