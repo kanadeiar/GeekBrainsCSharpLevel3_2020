8e631289397b4d1cd89878375fad62841fdd4efd
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-17 15:09:35 +0300

    Добавление валидации данных сервера

--- HomeWorks/MailSender.lib/Models/Server.cs
+++ HomeWorks/MailSender.lib/Models/Server.cs
@@ -1,8 +1,11 @@
-﻿using MailSender.Models.Base;
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using MailSender.Models.Base;
 
 namespace MailSender.Models
 {
-    public class Server : NamedModel
+    public class Server : NamedModel, IDataErrorInfo
     {
         private string _name;
         public override string Name 
@@ -32,5 +35,56 @@ namespace MailSender.Models
         public string Login { get; set; }
         public string Password { get; set; }
 
+        string IDataErrorInfo.Error => null;
+
+        
+        public string this[string propertyName]
+        {
+            get
+            {
+                Regex regex = null;
+                switch (propertyName)
+                {
+                    case nameof(Name):
+                        var name = Name;
+                        if (name is null) return "Имя сервера не может быть пустой строкой";
+                        if (name.Length < 3) return "Имя сервера не может быть короче трех симоволов";
+                        if (name.Length > 30) return "Имя сервера не может быть длиннее тридцати символов";
+                        return null;
+                    case nameof(Address):
+                        var address = Address;
+                        if (address is null) return "Адрес сервера не может быть пустой строкой";
+                        regex = new Regex(@"(\w+\.)*\w+[A-Za-z]+");
+                        if (!regex.IsMatch(address)) return "Строка адреса сервера имеет неверный формат";
+                        return null;
+                    case nameof(Port):
+                        var port = Port;
+                        if (port < 1) return "Значение порта не может быть меньше одного";
+                        if (port > 999) return "Значение порта не может быть больше 999";
+                        return null;
+                    case nameof(Desctiption):
+                        var description = Desctiption;
+                        if (description == "Туфта") return "Запрещено вводить такое слово как \"Туфта\"!";
+                        return null;
+                    case nameof(Login):
+                        var login = Login;
+                        if (login is null) return "Значение логина пользователя не может быть пустой строкой";
+                        if (login.Length < 3) return "Слишком короткое имя пользователя";
+                        if (login.Length > 30) return "Слишком длинное имя пользователя";
+                        regex = new Regex(@"(?i)\b(\w+\p{P}*\p{S}*\p{Z}*\p{C}*\s?)+\b");
+                        if (!regex.IsMatch(login)) return "Логин пользователя слишком простой";
+                        return null;
+                    case nameof(Password):
+                        var password = Password;
+                        if (password is null) return "Значение пароля не может быть пустым";
+                        if (password.Length < 6) return "Слишком короткий пароль";
+                        regex = new Regex(@"(\w+\.)*\w+[A-Za-z]+");
+                        if (!regex.IsMatch(password)) return "Пароль слишком простой, должны быть: цифры, спецсимволы, буквы в верхнем регистре, нижнем регистре и длинна более 6 символов";
+                        return null;
+                    default:
+                        return null;
+                }
+            }
+        }
     }
 }
--- HomeWorks/WpfMailSenderCore/Windows/ServerEditWindow.xaml.cs
+++ HomeWorks/WpfMailSenderCore/Windows/ServerEditWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using MailSender.Models;
 
 namespace WpfMailSenderCore.Windows
 {
@@ -10,6 +11,7 @@ namespace WpfMailSenderCore.Windows
     /// </summary>
     public partial class ServerEditWindow : Window
     {
+        public Server Server { get; set; }
         public ServerEditWindow()
         {
             InitializeComponent();
@@ -19,36 +21,49 @@ namespace WpfMailSenderCore.Windows
             DialogResult = !((Button)e.OriginalSource).IsCancel;
             Close();
         }
-        private void TextBoxServerPort_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            if (!(sender is TextBox textBox) || textBox.Text == "") return;
-            e.Handled = !int.TryParse(textBox.Text, out _);
-        }
         public static bool ShowDialog(string title, ref string server, ref string address, ref int port, ref bool useSSL,
             ref string description, ref string login, ref string password)
         {
             var window = new ServerEditWindow
             {
                 Title = title,
-                TextBoxServerName = { Text = server },
-                TextBoxServerAddress = { Text = address },
-                TextBoxServerPort = { Text = port.ToString() },
-                CheckBoxServerSSL = { IsChecked = useSSL },
-                TextBoxDescription = { Text = description },
-                TextBoxLogin = { Text = login },
-                PasswordBoxPassword = { Password = password },
+                Server = new Server
+                {
+                    Name = server,
+                    Address = address,
+                    Port = port,
+                    UseSSL = useSSL,
+                    Desctiption = description,
+                    Login = login,
+                    Password = password,
+                },
+                //TextBoxServerName = { Text = server },
+                //TextBoxServerAddress = { Text = address },
+                //TextBoxServerPort = { Text = port.ToString() },
+                //CheckBoxServerSSL = { IsChecked = useSSL },
+                //TextBoxDescription = { Text = description },
+                //TextBoxLogin = { Text = login },
+                //PasswordBoxPassword = { Password = password },
                 Owner = Application.Current.Windows.Cast<Window>()
                     .FirstOrDefault(win => win.IsActive),
             };
+            window.DockPanelServerEdit.DataContext = window.Server;
             if (window.ShowDialog() != true)
                 return false;
-            server = window.TextBoxServerName.Text;
-            address = window.TextBoxServerAddress.Text;
-            port = int.Parse(window.TextBoxServerPort.Text);
-            useSSL = window.CheckBoxServerSSL.IsChecked == true;
-            description = window.TextBoxDescription.Text;
-            login = window.TextBoxLogin.Text;
-            password = window.PasswordBoxPassword.Password;
+            server = window.Server.Name;
+            address = window.Server.Address;
+            port = window.Server.Port;
+            useSSL = window.Server.UseSSL;
+            description = window.Server.Desctiption;
+            login = window.Server.Login;
+            password = window.Server.Password;
+            //server = window.TextBoxServerName.Text;
+            //address = window.TextBoxServerAddress.Text;
+            //port = int.Parse(window.TextBoxServerPort.Text);
+            //useSSL = window.CheckBoxServerSSL.IsChecked == true;
+            //description = window.TextBoxDescription.Text;
+            //login = window.TextBoxLogin.Text;
+            //password = window.PasswordBoxPassword.Password;
             return true;
         }
         public static bool Create(out string server, out string address, out int port, out bool useSSL, out string description,
