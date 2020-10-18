183bf76c0113cce2ed995aac69eaa52db182254f
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-18 11:20:57 +0300

    Первый тест

---
+++ HomeWorks/Tests/MailSender.lib.Tests1/Service/TextEncryptorTests.cs
@@ -0,0 +1,32 @@
+﻿using MailSender.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests1.Service
+{
+    [TestClass]
+    public class TextEncryptorTests
+    {
+        [TestMethod]
+        public void Encode_ABC_to_BCD_with_key_1()
+        {
+            const string str = "ABC";
+            const int key = 1;
+            const string expectedStr = "BCD";
+
+            var actualStr = TextEncoder.Encode(str, key);
+
+            Assert.AreEqual(expectedStr, actualStr);
+        }
+        [TestMethod]
+        public void Decode_BCD_to_ABC_with_key_1()
+        {
+            const string str = "BCD";
+            const int key = 1;
+            const string expectedStr = "ABC";
+
+            var actualStr = TextEncoder.Decode(str, key);
+
+            Assert.AreEqual(expectedStr, actualStr);
+        }
+    }
+}
