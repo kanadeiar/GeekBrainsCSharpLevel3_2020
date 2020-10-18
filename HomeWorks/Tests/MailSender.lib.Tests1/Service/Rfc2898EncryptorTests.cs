53216411aaaca034bae9d5c36a7068fc307edc71
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-18 12:11:46 +0300

    Тесты шифровальщика работают

---
+++ HomeWorks/Tests/MailSender.lib.Tests1/Service/Rfc2898EncryptorTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using MailSender.Interfaces;
+using MailSender.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests1.Service
+{
+    [TestClass]
+    public class Rfc2898EncryptorTests
+    {
+        private IEncryptService _encrypt = new Rfc2898Encryptor();
+        [TestMethod]
+        public void Encrypt_Hello_And_Decrypt_with_Password()
+        {
+            const string str = "Hello World!";
+            const string password = "Password";
+
+            var encryptedStr = _encrypt.Encrypt(str, password);
+            var decryptedStr = _encrypt.Decrypt(encryptedStr, password);
+
+            Assert.AreNotEqual(str, encryptedStr);
+            Assert.AreEqual(str, decryptedStr);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(CryptographicException))]
+        public void Wrong_Decryption_thrown_CryptographicException()
+        {
+            const string str = "Hello World!";
+            const string password = "Password";
+
+            var encryptedStr = _encrypt.Encrypt(str, password);
+            var wrong = _encrypt.Decrypt(encryptedStr, "123");
+        }
+    }
+}
